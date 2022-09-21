a4eebfa98b6c04cdb584a133e9c3e8fab0f7bdc5
flaviusgabriel <62425734+flaviusgabriel@users.noreply.github.com>
2022-09-21 10:04:55 +0300

    add existing file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeaderNav from "./components/header-nav/HeaderNav";
+import Hero from "./components/hero/Hero";
+
+function App() {
+  return (
+    <div>
+      <HeaderNav />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
